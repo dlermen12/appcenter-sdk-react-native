47bbc2ff3d3eb9c02d80b224281191cc5891a4e0
Denner <dennerl@hotmail.com>
2019-07-14 23:27:34 +0100

    remove R.

--- appcenter-crashes/react-native.config.js
+++ appcenter-crashes/react-native.config.js
@@ -3,7 +3,7 @@ module.exports = {
     platforms: {
       ios: {},
       android: {
-        packageInstance: 'new AppCenterReactNativeCrashesPackage(getApplication(), getResources().getString(R.string.appCenterCrashes_whenToSendCrashes))'
+        packageInstance: 'new AppCenterReactNativeCrashesPackage(getApplication(), "ASK_JAVASCRIPT")'
       }
     }
   }
